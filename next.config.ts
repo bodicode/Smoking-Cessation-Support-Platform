a2edd66858fba0e2f1a4aacbc1482211d720249c
Bo <29.hoang.10@gmail.com>
2025-05-30 15:03:54 +0700

    fix: leaflet for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
-// next.config.js
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-
 const nextConfig = {
-    webpack: (config: any) => {
-        config.cache = false;
-        return config;
-    },
+  webpack: (config: any) => {
+    config.cache = false;
+    return config;
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
